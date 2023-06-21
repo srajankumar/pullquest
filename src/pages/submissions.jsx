97999093135e3e3f2e8ca937e58839ef5b439915
srajankumar <kumarsrajan02@gmail.com>
2023-06-21 20:29:01 +0530

    copy button working :)

--- src/pages/submissions.jsx
+++ src/pages/submissions.jsx
@@ -3,9 +3,9 @@ import data from "../../data.json";
 
 const submissions = () => {
   return (
-    <div>
+    <div className="container flex flex-wrap text-justify justify-center md:p-10 p-5 mb-10">
       {Object.values(data).map((item, index) => (
-        <div key={index}>
+        <div className="bg-green-400 m-3 w-80" key={index}>
           <h2>{item.name}</h2>
           <p>Age: {item.age}</p>
           <p>Email: {item.email}</p>
