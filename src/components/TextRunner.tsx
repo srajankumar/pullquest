ada5a4827b5faf162d527398c219ed399a58e0e9
srajankumar <kumarsrajan02@gmail.com>
2023-06-27 19:19:20 +0530

    final testing

---
+++ src/components/TextRunner.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { Cursor, useTypewriter } from "react-simple-typewriter";
+
+type Props = {};
+
+const TextRunner = (props: Props) => {
+  const [text, count] = useTypewriter({
+    words: ["Click here to Begin the Quest"],
+    loop: true,
+    delaySpeed: 1500,
+    deleteSpeed: 50,
+  });
+  return (
+    <h1 className="py-5">
+      <span>{text}</span>
+      <Cursor
+        cursorStyle="->"
+        cursorColor="rgb(216 180 254 / var(--tw-bg-opacity))"
+        cursorBlinking={true}
+      />
+    </h1>
+  );
+};
+
+export default TextRunner;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Image from "next/image";
 import waves from "../assets/svg.png";
 import Link from "next/link";
+import TextRunner from "@/components/TextRunner";
 
 export default function Home() {
   return (
@@ -11,13 +12,10 @@ export default function Home() {
         className="absolute object-cover h-screen opacity-100 bottom-0 -z-10"
         alt="img"
       />
-      <h1 className="fixed flex flex-col text-white  text-xl md:pb-20 pb-36 font-extrabold justify-center items-center h-full w-full">
+      <h1 className="fixed font-sans flex flex-col text-white text-xl md:pb-20 pb-36 font-extrabold justify-center items-center h-full w-full">
         <div className="sm:text-7xl text-6xl font-sans">Pull Quest</div>
         <Link href="/instructions">
-          <img
-            src="https://readme-typing-svg.demolab.com?size=15&pause=1000&color=FFFFFF&center=true&vCenter=true&width=435&height=60&lines=Click+Here+to+begin+the+Quest+-%3E"
-            alt="Typing SVG"
-          />
+          <TextRunner />
         </Link>
       </h1>
     </div>
