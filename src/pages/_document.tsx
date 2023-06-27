adbf9789435296953bb3bbbc53302c413f3e5a0d
srajankumar <kumarsrajan02@gmail.com>
2023-06-27 21:36:42 +0530

    title added

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Pull Quest</title>
+      </Head>
       <body>
         <Navbar />
         <Main />
