55e17ca1e524e8cfa3ec165768be8690d313ea92
premsaivarmachekuri <premsaivarmacoding@gmail.com>
2023-10-09 20:17:02 +0530

    updated the background image

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -7,7 +7,7 @@ const Background = () => {
     <div>
       <Image
         src={waves}
-        className="absolute object-cover h-screen opacity-100 bottom-0 -z-20 w-full"
+        className="fixed object-cover h-screen opacity-50 bottom-0 -z-10 w-full"
         alt="img"
       />
     </div>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,12 +3,15 @@ import Image from "next/image";
 import waves from "../assets/svg.png";
 import Link from "next/link";
 import TextRunner from "@/components/TextRunner";
-import Background from "../components/Background";
 
 export default function Home() {
   return (
     <div>
-      <Background />
+            <Image
+        src={waves}
+        className="absolute object-cover h-screen opacity-100 bottom-0 -z-20 w-full"
+        alt="img"
+      />
       <h1 className="fixed font-sans flex flex-col text-white text-xl -z-10 md:pb-20 pb-36 font-extrabold justify-center items-center h-full w-full">
         <div className="sm:text-7xl text-6xl font-sans">Pull Quest</div>
         <Link href="/instructions">
